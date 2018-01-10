a17a599504fdadc4760282e8e8d7450fee9001c1
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-10 04:11:38 -0500

    set up initial home page

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const hbs = require('express-handlebars')
 
 const app = express()
+app.set('view engine', 'hbs')
+
+app.engine('.hbs', hbs({
+  exttname: '.hbs',
+  partialsDir: 'views/',
+  layoutsDir: 'views/',
+  defaultLayout: 'home'
+}))
 
 app.set('port', process.env.PORT || 9000)
 
